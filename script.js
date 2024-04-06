81e24464c572bc4626f1d0b84c70c136b6c9ce3e
Nikhil2005Rai <n2005rai@gmail.com>
2024-04-06 15:12:43 +0530

    all done

---
+++ script.js
@@ -0,0 +1,30 @@
+//Generate random color
+function getRandomColor(){
+    const hex = "0123456789ABCDEF"
+    let color = "#"
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random()*16)]
+    }
+    return color
+}
+
+let interValid
+
+startChangingColor = function(){
+    interValid = setInterval(() => {
+        let newColor = getRandomColor()
+        console.log(newColor)
+        document.body.style.backgroundColor = newColor
+    }, 1000); 
+}
+
+stopChangingColor = function(){
+    clearInterval(interValid)
+}
+
+const startInterval = document.querySelector("#start")
+const stopInterval = document.querySelector("#stop")
+
+startInterval.addEventListener("click",startChangingColor)
+
+stopInterval.addEventListener("click",stopChangingColor)
